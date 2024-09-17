dfa7a5822d72f6131dc21362226c200fe5243bbe
Ruth Keane <Ruth.Keane1@udal.nhs.uk>
2024-09-17 18:12:07 +0100

    make blank space for functions - unfinished

--- functions.R
+++ functions.R
@@ -68,61 +68,8 @@ expand_categorical_filters <- function(string,
     str_replace_all("c\\(", "\\(") #remove c from the start of vectors 
   #loop through all filters, replacing  filter with full text version of filter
   for (i in list_of_filters) {
-    
-      #split each filter into column, value and operator
-      column <- as.character(i[2])
-      value <- i[[3]]
-      operator <- as.character(i[1])
-      
-       #get column name
-      column_searchable<- column %>%
-        str_extract("[a-zA-Z0-9_.-]+") #pull out just column code
-      column_new <- get_column_text(column_searchable, dataset)
-
-       
-      #create vector for value
-      value_old <- c()
-      value_new <- c()
-      
-      # get value descriptions
-      # loop through each element in the vector (so can handle 1 or c(1,2,3))
-      for (j in 1:length(value)) {
-        #this is required because value is of the type class when it is in c(1,2,3) form
-        if (value[[j]]!="c"){
-          j_value<- value[[j]]
-          j_value_pretty <- j_value %>% 
-            str_replace_all("% ","") %>% #get rid of "% "- which is present with a multi-select column
-            str_replace_all(" %", "") #get rid of " %"- which is present with a multi-select column
-          
-          #add old value to vector
-          value_old <- append(value_old, j_value)
-          #get text for this element of value 
-          code_text <- get_code_text(column_searchable, j_value_pretty, dataset)
-          #append this value to a vector of values
-          value_new <- append(value_new, code_text)
-        }
-      }
-      
-      #convert from vector to string
-      value_old <- str_c(value_old, collapse = ", ")
-      value_new_string <- str_c(value_new, collapse = ", ")
-      
-      #formatting is a little different when value has one or more elements
-      if (length(value_new) > 1) {
-        # add brackets to either side of value_old - to allow matching to occur
-        value_old <- str_c("\\(", value_old, "\\)")
-        # add brackets to either side of value_new_string- helpful for clarity
-        value_new_string <- str_c("(", value_new_string, ")")
-      }
-      
-      #recreate the filter by combining column, operator and value old
-      filter_initial <- str_c(column_searchable, operator, value_old , sep = " ")
-      filter_nice <- str_c(column_new, operator, value_new_string, sep = " ")
-  
-      string_formatted <-  string_formatted %>% 
-        #replace initial filter with the formatted filter
-        str_replace_all(filter_initial, filter_nice)
-             }
+   string_formatted<- replace_filter(string_formatted, i, dataset)
+  }
   #replace |, & , ==, like and %in% with more understandable phrases
   string_formatted<- string_formatted %>%  
     str_replace_all("\\|", "OR") %>%
@@ -133,4 +80,77 @@ expand_categorical_filters <- function(string,
   print(string_formatted)
   
   return(string_formatted)
+}
+
+
+
+replace_filter<-function(string_formatted, i, dataset){
+
+  filter_category<-find_filter_category(i)
+  
+  if (filter_category=="in"){
+    
+  }else if (filter_category=="equals"){
+    
+  }else if (filter_category=="multi-select"){
+    
+  }else if (filter_category=="na"){
+    
+  }
+    
+    
+  #split each filter into column, value and operator
+  column <- as.character(i[2])
+  value <- i[[3]]
+  operator <- as.character(i[1])
+  
+  #get column name
+  column_searchable<- column %>%
+    str_extract("[a-zA-Z0-9_.-]+") #pull out just column code
+  column_new <- get_column_text(column_searchable, dataset)
+  
+  
+  #create vector for value
+  value_old <- c()
+  value_new <- c()
+  
+  # get value descriptions
+  # loop through each element in the vector (so can handle 1 or c(1,2,3))
+  for (j in 1:length(value)) {
+    #this is required because value is of the type class when it is in c(1,2,3) form
+    if (value[[j]]!="c"){
+      j_value<- value[[j]]
+      j_value_pretty <- j_value %>% 
+        str_replace_all("% ","") %>% #get rid of "% "- which is present with a multi-select column
+        str_replace_all(" %", "") #get rid of " %"- which is present with a multi-select column
+      
+      #add old value to vector
+      value_old <- append(value_old, j_value)
+      #get text for this element of value 
+      code_text <- get_code_text(column_searchable, j_value_pretty, dataset)
+      #append this value to a vector of values
+      value_new <- append(value_new, code_text)
+    }
+  }
+  
+  #convert from vector to string
+  value_old <- str_c(value_old, collapse = ", ")
+  value_new_string <- str_c(value_new, collapse = ", ")
+  
+  #formatting is a little different when value has one or more elements
+  if (length(value_new) > 1) {
+    # add brackets to either side of value_old - to allow matching to occur
+    value_old <- str_c("\\(", value_old, "\\)")
+    # add brackets to either side of value_new_string- helpful for clarity
+    value_new_string <- str_c("(", value_new_string, ")")
+  }
+  
+  #recreate the filter by combining column, operator and value old
+  filter_initial <- str_c(column_searchable, operator, value_old , sep = " ")
+  filter_nice <- str_c(column_new, operator, value_new_string, sep = " ")
+  
+  string_formatted <-  string_formatted %>% 
+    #replace initial filter with the formatted filter
+    str_replace_all(filter_initial, filter_nice)
+
 }
