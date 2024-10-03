f226663127ca03c7c744435ea911f105e4b61bf7
Ruth Keane <Ruth.Keane1@udal.nhs.uk>
2024-10-03 11:16:59 +0100

    comment functions script- and small change to improve efficiency

--- functions.R
+++ functions.R
@@ -151,14 +151,14 @@ replace_filter <- function(string_formatted, i, dataset) {
       value_string <- str_replace_all(as.character(i[[3]]), '\"', "")
       #converting c(1,2,3) into a character includes the letter "c" - this must be removed
       value_string <- value_string[value_string != "c"]
-      # convert to a string (to make consistent with next section)
+      # convert to a string (to make consistent with next section) - there are already spaces here so use "," not ", "
       value_string <- str_c(value_string, collapse = ",")
       not_string <- ""
     } else{
-      #the first element is the ! so the second element has the "in"
+      #the first element is the ! so the second element has the rest of the query
       i_second <- as.character(i)[2]      
       i_second <- str_replace_all(i_second, 'c\\(|\\)|\"', "")
-      #pull out the column name (lhs of %in%)
+      #pull out the column name (lhs of  %in% )
       column <- str_split_i(i_second, " %in% ", 1)
       #pull out the c(1,2,3) (rhs of %in%)
       value_string <- str_split_i(i_second, " %in% ", 2)
@@ -167,9 +167,10 @@ replace_filter <- function(string_formatted, i, dataset) {
     }
     # split into vector
     value_vec_old <- str_split(value_string, ",")[[1]]
-    value_vec_old <- as.character(value_vec_old)
+    value_vec_old <- as.character(value_vec_old) #make sure each element is a character, not string
     #find column name
     column_new <- get_column_text(column, dataset)
+    
     #find the new values
     value_vec_new <- map_chr(value_vec_old, ~ get_code_text(column, .x, dataset))
     
@@ -185,36 +186,52 @@ replace_filter <- function(string_formatted, i, dataset) {
     filter_nice <- str_c(column_new, " ", not_string, "%in% ", value_new)
     
   } else if (filter_category == "equals") {
+    #column name is second element of i 
     column <- as.character(i[2])
+    # convert column code to column name
     column_new <- get_column_text(column, dataset)
+    # value is third item of i
     value_old <- i[[3]]
+    # operator is first item of i
     operator <- as.character(i[1])
+    #convert value to full version
     value_new_string <- get_code_text(column, value_old, dataset)
     #recreate the filter by combining column, operator and value old
     filter_initial <- str_c(column, operator, value_old , sep = " ")
     filter_nice <- str_c(column_new, operator, value_new_string, sep = " ")
     
   } else if (filter_category == "multi") {
+    # this has an awkward structure " " + A001 + " " - so pull out just the column name
     column <- as.character(i[2]) %>%
       str_extract("[a-zA-Z0-9_.-]+")
+    # convert column name to full version
     column_new <- get_column_text(column, dataset)
     
+    #value is third element
     value_old <- i[[3]]
+    # operator is first element
     operator <- as.character(i[1])
     
+    #% symbol is required in search - remove it to make output more clear and allow conversion of value 
     value_pretty <- value_old %>%
       str_replace_all("% ", "") %>% #get rid of "% "- which is present with a multi-select column
       str_replace_all(" %", "") #get rid of " %"- which is present with a multi-select column
+    
+    #get code text
     value_new_string <- get_code_text(column, value_pretty, dataset)
     #recreate the filter by combining column, operator and value old
     filter_initial <- str_c(column, operator, value_old , sep = " ")
     filter_nice <- str_c(column_new, operator, value_new_string, sep = " ")
     
   } else if (filter_category == "filter_na") {
+    #work out if this is an !is.na or is.na- flag if it is not na
     not_na <- as.character(i)[1] == "!"
+    # pull out column code
     column <- as.character(i)[2] %>%
       str_extract_all("[a-zA-Z0-9_-]{3,}")
+    #convert column name
     column_new <- get_column_text(column , dataset)
+    #build up new version of filter, depending on whether the search is !is.na or is.na
     if (not_na) {
       filter_initial <- str_glue("!is.na\\({column}\\)")
       filter_nice <- str_glue("{column_new} IS NOT NA")
@@ -224,7 +241,7 @@ replace_filter <- function(string_formatted, i, dataset) {
     }
     
   }
-  
+  # replace filter initial with filter_nice. if no filter category was found, the replace will not run
   if (!is.na(filter_category)) {
     string_formatted <-  string_formatted %>%
       #replace initial filter with the formatted filter
@@ -235,92 +252,139 @@ replace_filter <- function(string_formatted, i, dataset) {
 }
 
 
+#' find filter category
+#' 
+#' Finds the category of a filter - can be equals, in , not in, filter_na, multi or NA if not possible to categorise
+#'
+#' @param i filter to find category of (call)
+#'
+#' @return string representing category (or NA)
 find_filter_category <- function(i) {
+  
+  # look for specific patterns found in the different categories
   like_present <- sum(str_detect(as.character(i), "%LIKE%")) == 1
   space_number_space_present <- sum(str_detect(as.character(i), "% \\d+ %")) == 1
   space_colname_space_present <- sum(str_detect(as.character(i), '" " *\\+ *\\w+ *\\+ *" "')) == 1
   space_colname_space_present <- sum(str_detect(as.character(i), '" " *\\+ *\\w+ *\\+ *" "')) == 1
-  
   vector_present <- sum(str_detect(as.character(i), "c\\(")) == 1
   in_present <- sum(str_detect(as.character(i), "%in%")) == 1
   not_at_start <- sum(as.character(i) == "!") == 1
   equals_present <- sum(str_detect(as.character(i), "==|!=")) == 1
   
-  
+  #work out if criteria for any category is met
   type_na <- sum(str_detect(as.character(i), "na"))
-  
   type_equals <- if_else(equals_present & !vector_present, 1, 0)
-  
   type_in <- if_else(vector_present &
                        in_present & !not_at_start, 1, 0)
-  
   type_not_in <- if_else(vector_present &
                            in_present & not_at_start, 1, 0)
-  
   type_multi <- if_else(like_present &
                           space_number_space_present &
-                          space_colname_space_present,
-                        1,
-                        0)
+                          space_colname_space_present, 1, 0)
   
-  filter_category <- NA
-  
-  if (type_equals == 1  &
-      sum(type_in, type_not_in, type_na, type_multi) == 0) {
+  #set filter category if meet ONLY ONE category
+  if (type_equals == 1 & sum(type_in, type_not_in, type_na, type_multi) == 0) {
     filter_category = "equals"
   }
-  else if (type_in == 1  &
-           sum(type_not_in, type_equals, type_na, type_multi) == 0) {
+  else if (type_in == 1  & sum(type_not_in, type_equals, type_na, type_multi) == 0) {
     filter_category = "in"
   }
-  else if (type_not_in == 1  &
-           sum(type_in, type_equals, type_na, type_multi) == 0) {
+  else if (type_not_in == 1 & sum(type_in, type_equals, type_na, type_multi) == 0) {
     filter_category = "not_in"
   }
-  else if (type_na == 1  &
-           sum(type_in, type_not_in, type_equals, type_multi) == 0) {
+  else if (type_na == 1 & sum(type_in, type_not_in, type_equals, type_multi) == 0) {
     filter_category = "filter_na"
   }
-  else if (type_multi == 1  &
-           sum(type_in, type_not_in, type_na, type_equals) == 0) {
+  else if (type_multi == 1 & sum(type_in, type_not_in, type_na, type_equals) == 0) {
     filter_category = "multi"
   } else{
+    filter_category = NA 
     print("Filter category not found")
   }
   return(filter_category)
 }
 
+#' add summary sheet
+#'
+#'searches environment for summary_categories_lfpse, nrls or steis, depending on database_name
+#'creates summary tables from the columns specified by this object
+#'
+#' @param wb workbook (workbook class)
+#' @param i name of dataset (string)
+#' @param title title of sheet
+#' @param database_name name of database (i.e. lfpse, steis or nrls)
+#' @param sheet sheet name
+#'
+#' @return workbook with summary tables added
 add_summary_sheet <- function(wb, i, title, database_name, sheet) {
+  
   summary_categories_list <- get(str_glue("summary_categories_{database_name}"))
   
-  df <- get(i)
+  df <- get(i) #get dataset from environment
+  # add extra columns to df, depending on database name - refactor harm levels
+  if (database_name == "lfpse") {
+    df <- df %>%
+      mutate(
+        Year = year(`T005 - Event date`),
+        Month = month(`T005 - Event date`, label = TRUE, abbr = TRUE)
+      ) %>%
+      mutate(
+        `OT001 - Physical harm` = fct_relevel(
+          `OT001 - Physical harm`,
+          "No physical harm",
+          "Low physical harm",
+          "Moderate physical harm",
+          "Severe physical harm",
+          "Fatal"
+        )
+      )
+    
+  } else if (database_name == "nrls") {
+    df <- df %>%
+      mutate(
+        Year = year(`Date of Incident`),
+        Month = month(`Date of Incident`, label = TRUE, abbr = TRUE)
+      ) %>%
+      mutate(
+        `PD09 Degree of harm (severity)` = fct_relevel(
+          `PD09 Degree of harm (severity)`,
+          "No Harm",
+          "Low",
+          "Moderate",
+          "Severe",
+          "Death"
+        )
+      )
+    
+  } else if (database_name == "steis") {
+    print("steis")
+    df <- df %>%
+      mutate(
+        Year = year(`Created on`),
+        Month = month(`Created on`, label = TRUE, abbr = TRUE)
+      )
   
-  addWorksheet(wb, sheet, gridLines = FALSE)
-  
+  }
+  addWorksheet(wb, sheet, gridLines = FALSE) 
+   
   # set column widths
-  setColWidths(wb,
-               sheet = sheet,
-               cols = 1,
-               widths = 50)
-  #
+  setColWidths(wb, sheet = sheet, cols = 1, widths = 50)
   
   #Add text style
-  addStyle(wb,
-           sheet = sheet,
-           textStyle,
-           rows = 1:6,
-           cols = 1:1)
+  addStyle(wb, sheet = sheet,  textStyle, rows = 1:6, cols = 1:1)
   
-  
-  # Write text
-  writeData(
-    wb,
-    sheet,
-    paste(sheet, "Confidential", sep = " - "),
-    startCol = 1,
-    startRow = 1
+  # Write title
+  writeData( wb, 
+             sheet, 
+             paste(sheet, "Confidential", sep = " - "),
+             startCol = 1,
+             startRow = 1
   )
+  
+  #write subtitle
   writeData(wb, sheet, title, startCol = 1, startRow = 3)
+  
+  # write number of incidents
   writeData(
     wb,
     sheet,
@@ -329,66 +393,24 @@ add_summary_sheet <- function(wb, i, title, database_name, sheet) {
     startRow = 5
   )
   
-  
-  
+  #set start row for summary tables
   start_row = 7
   
+  # loop through list- each item of list is one table
   for (category in summary_categories_list) {
+    #work out if table has one or 2 variables
     if (length(category) == 1) {
       one_variable = TRUE
-      category[[2]] = category[[1]]
+      category[[2]] = category[[1]] #to allow code to work the same for 1 or 2 categories, add a category with the same column as the first
     } else{
       one_variable = FALSE
-    }
+      }
     
-    if (database_name == "lfpse") {
-      df <- df %>%
-        mutate(
-          Year = year(`T005 - Event date`),
-          Month = month(`T005 - Event date`, label = TRUE, abbr = TRUE)
-        ) %>%
-        mutate(
-          `OT001 - Physical harm` = fct_relevel(
-            `OT001 - Physical harm`,
-            "No physical harm",
-            "Low physical harm",
-            "Moderate physical harm",
-            "Severe physical harm",
-            "Fatal"
-          )
-        )
-      
-    } else if (database_name == "nrls") {
-      df <- df %>%
-        mutate(
-          Year = year(`Date of Incident`),
-          Month = month(`Date of Incident`, label = TRUE, abbr = TRUE)
-        ) %>%
-        mutate(
-          `PD09 Degree of harm (severity)` = fct_relevel(
-            `PD09 Degree of harm (severity)`,
-            "No Harm",
-            "Low",
-            "Moderate",
-            "Severe",
-            "Death"
-          )
-        )
-      
-    } else if (database_name == "steis") {
-      print("steis")
-      df <- df %>%
-        mutate(
-          Year = year(`Created on`),
-          Month = month(`Created on`, label = TRUE, abbr = TRUE)
-        )
-      
-    }
-    
+    #work out counts
     summary_table <- df %>%
       count(!!category[[1]], !!category[[2]])
-    print(summary_table)
-    
+
+    # if 2 variables add row and column totals
     if (!one_variable) {
       summary_table <- summary_table %>%
         pivot_wider(names_from = !!category[[2]],
@@ -396,16 +418,17 @@ add_summary_sheet <- function(wb, i, title, database_name, sheet) {
         adorn_totals('both')
       
     } else{
+      # if one variable, add row totals and percentage column
       summary_table <- summary_table %>%
         mutate(percent = scales::percent(n / sum(n))) %>%
         adorn_totals('row')
     }
     
     print(summary_table)
+    #add summary table to sheet
     writeData(wb, sheet, summary_table, startRow = start_row)
     
-    #
-    # # Add header style
+    # style table - header
     addStyle(
       wb,
       sheet = sheet,
@@ -413,7 +436,7 @@ add_summary_sheet <- function(wb, i, title, database_name, sheet) {
       rows = start_row,
       cols = 1:(ncol(summary_table))
     )
-    
+    # style table- row titles
     addStyle(
       wb,
       sheet = sheet,
@@ -421,8 +444,7 @@ add_summary_sheet <- function(wb, i, title, database_name, sheet) {
       rows = (start_row + 1):(nrow(summary_table) + start_row),
       cols = 1
     )
-    #
-    # # Add body style
+    #style table - main body
     addStyle(
       wb,
       sheet = sheet,
@@ -440,13 +462,23 @@ add_summary_sheet <- function(wb, i, title, database_name, sheet) {
       rows = start_row:(start_row + nrow(summary_table)),
       heights = 34
     )
-    
+    # increment start row to allow next table to be further down on page
     start_row <- start_row + nrow(summary_table) + 3
     
   }
   return(wb)
 }
 
+#' add data sheet
+#' 
+#' add data to sheet and style it
+#'
+#' @param wb workbook (workbook class)
+#' @param i name of dataset (string)
+#' @param title title of sheet
+#' @param sheet sheet name
+#'
+#' @return workbook with data added
 add_data_sheet <- function(wb, i, title, sheet) {
   df <- get(i)
   
