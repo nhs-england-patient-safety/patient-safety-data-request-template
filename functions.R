d68dca334e1c30e1df2a35ee7bd5eb052867a92f
ruth-keane-nhs <ruth.keane1@nhs.net>
2024-11-08 12:55:41 +0000

    code to create lfpse data for summary table ok

--- functions.R
+++ functions.R
@@ -1,3 +1,8 @@
+#function to find the minimum number in a vector, and return NA if all values are NA
+min_safe<- function(vec){
+  ifelse(length(vec[!is.na(vec)]) == 0, NA_real_, min(vec,na.rm=TRUE))
+}
+
 #' add summary sheet
 #'
 #'searches environment for summary_categories_lfpse, nrls or steis, depending on database_name
