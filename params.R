e864780036f064de0d2fe600590ccd25c95bbffb
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-03-14 11:14:38 +0000

    shorten messages in params.R

--- params.R
+++ params.R
@@ -43,12 +43,9 @@ steis_filename <- 'SUI_2_87360.csv'
 expanded_categorical_filter_lfpse<-translate_categorical_string(lfpse_categorical, "lfpse")
 expanded_categorical_filter_nrls<-translate_categorical_string(nrls_categorical, "nrls")
 expanded_categorical_filter_steis<-translate_categorical_string(steis_categorical, "steis")
-message("LFPSE filter is:")
-message(expanded_categorical_filter_lfpse)
-message("NRLS filter is:")
-message(expanded_categorical_filter_nrls)
-message("StEIS filter is:")
-message(expanded_categorical_filter_steis)
+message(str_glue("LFPSE filter is: \n{expanded_categorical_filter_lfpse}"))
+message(str_glue("NRLS filter is: \n{expanded_categorical_filter_nrls}"))
+message(str_glue("StEIS filter is: \n{expanded_categorical_filter_steis}"))
 
 # text terms
 #example below- not real example
