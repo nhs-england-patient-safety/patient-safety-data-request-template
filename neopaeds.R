8ade488bbdf0d94a18cfa9d538ca8a7179d56a7c
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-04-14 20:09:01 +0100

    "orthogeriatric" not being picked up, so removed break at start of geriat

--- neopaeds.R
+++ neopaeds.R
@@ -3,4 +3,4 @@ neonatal_terms <- "(?i)\\bn(?:|\\W)i(?:|\\W)c(?:|\\W)u\\b|\\bn(?:|\\W)n(?:|\\W)u
 neonatal_specialty_terms <- "(?i)\\b(neonat|baby)"
 paediatric_terms <- "(?i)\\bp(?:|\\W)i(?:|\\W)c(?:|\\W)u\\b|\\bc(?:|\\W)a(?:|\\W)m(?:|\\W)h(?:|\\W)s\\b|\\bpaed|\\binfant|\\bschool\\bchild"
 paediatric_specialty_terms <- "(?i)\\bpaed|\\bchild"
-adult_specialty_terms <-  "(?i)\\badult|\\bold|\\belderly|\\bgeriat"
+adult_specialty_terms <-  "(?i)\\badult|\\bold|\\belderly|geriat"
