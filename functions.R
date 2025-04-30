2bd4df96cc14100d70efd696d6aaeffc4ce0fb32
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-04-30 20:01:44 +0100

    small change to make_text_terms_pretty

--- functions.R
+++ functions.R
@@ -602,7 +602,7 @@ make_text_terms_pretty <- function(term){
     str_replace_all(pattern = "\\|", " OR ") |>
     str_replace_all(pattern = fixed('\\b'), "%" ) |>
     str_replace_all(pattern = fixed('(?i)'), "" ) |>
-    str_replace("term_", "term: ") |>
+    str_replace_all("term_", "term: ") |>
     str_replace_all("group_", "Group ") |>
     str_replace_all("_", " ")
 }
