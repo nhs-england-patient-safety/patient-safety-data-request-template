a6c4baa2299e7d7923686ee47850648ed114a7e2
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-05-08 16:52:14 +0100

    Improve output for neopaeds

--- formatter.R
+++ formatter.R
@@ -118,34 +118,57 @@ writeData(wb, "Search strategy", metadata, startRow = 2, startCol = 2)
 writeData(wb, "Search strategy", metadata_answers, startRow = 2, startCol = 5)
 
 
+neopaed_logic<-c()
 
+if (is_neopaed != "none"){
+  writeData(wb, "Search strategy", "Neopaed logic:", startRow = 28, startCol = 2)
+  
+}
 
-if (is_neopaed!="none"){
-
+if (is_neopaed %in% c("either","neonate")){
 
   neopaed_logic<-
-    c(
-    "Neonate logic:",
-    str_glue("Neonate by age (NRLS): Age is between 0 and 28 days"),
-    str_glue("Neonate by specialty (NRLS): Specialty is Neonatology"),
-    str_glue("Neonate by text (NRLS): Specialty is 'Obstetrics and gynaecology' or PD04 is 'A paediatrics specialty' or PD20 is 'Yes' and text contains {make_text_terms_pretty(neonatal_terms)}"),
-    str_glue("Neonate by age (LFPSE): Age (or age category) is between 0 and 28 days"),
-    str_glue("Neonate by specialty (LFPSE): Specialty contains {make_text_terms_pretty(neonatal_specialty_terms)}"),
-    str_glue("Neonate by text (LFPSE):  Specialty does not contain {make_text_terms_pretty(adult_specialty_terms)} and text contains {make_text_terms_pretty(neonatal_terms)}"),
-    "Paediatric logic:",
-    str_glue("Paediatric by age (NRLS): Age is between 28 days and 18 years"),
-    str_glue("Paediatric by specialty (NRLS): Specialty is 'Child and adolescent mental health' (with unknown age), 'Community paediatrics' or 'Paedodontics'"),
-    str_glue("Paediatric by text (NRLS): PD04 is 'A paediatrics specialty' or PD20 is 'Yes' and text contains {make_text_terms_pretty(paediatric_terms)}"),
-    str_glue("Paediatric by age (LFPSE): Age (or age category) is  between 28 days and 18 years"),
-    str_glue("Paediatric by specialty (LFPSE): Specialty contains {make_text_terms_pretty(paediatric_specialty_terms)}"),
-    str_glue("Paediatric by text (LFPSE):  Specialty does not contain {make_text_terms_pretty(adult_specialty_terms)} and text contains {make_text_terms_pretty(paediatric_terms)}")
-)
-  writeData(wb, "Search strategy", "Neopaed logic:", startRow = 28, startCol = 2)
-  writeData(wb, "Search strategy", neopaed_logic, startRow = 28, startCol = 5)
-
-  addStyle(wb, "Search strategy", textStyle, rows = 28, cols = 5)
-  addStyle(wb, "Search strategy", textStyle, rows = 35, cols = 5)
+    c(neopaed_logic,
+      "Neonate logic:",
+      "NRLS:",
+      str_glue("Neonate by age (NRLS): Age is between 0 and 28 days"),
+      str_glue("Neonate by specialty (NRLS): Specialty is Neonatology"),
+      str_glue("Neonate by text (NRLS): Specialty is 'Obstetrics and gynaecology' or PD04 is 'A paediatrics specialty' or PD20 is 'Yes' and text contains {make_text_terms_pretty(neonatal_terms)}"),
+      "",
+      "LFPSE:",
+      str_glue("Neonate by age (LFPSE): Age (or age category) is between 0 and 28 days"),
+      str_glue("Neonate by specialty (LFPSE): Specialty contains {make_text_terms_pretty(neonatal_specialty_terms)}"),
+      str_glue("Neonate by text (LFPSE):  Specialty does not contain {make_text_terms_pretty(adult_specialty_terms)} and text contains {make_text_terms_pretty(neonatal_terms)}"),
+      "",
+      ""
+      )
+  
+  
 }
+
+if (is_neopaed %in% c("either","paed")){
+  neopaed_logic<-
+    c(neopaed_logic,   
+      "Paediatric logic:",
+      "NRLS:",
+      str_glue("Paediatric by age (NRLS): Age is between 28 days and 18 years"),
+      str_glue("Paediatric by specialty (NRLS): Specialty is 'Child and adolescent mental health' (with unknown age), 'Community paediatrics' or 'Paedodontics'"),
+      str_glue("Paediatric by text (NRLS): PD04 is 'A paediatrics specialty' or PD20 is 'Yes' and text contains {make_text_terms_pretty(paediatric_terms)}"),
+      "",
+      "LFPSE:",
+      str_glue("Paediatric by age (LFPSE): Age (or age category) is  between 28 days and 18 years"),
+      str_glue("Paediatric by specialty (LFPSE): Specialty contains {make_text_terms_pretty(paediatric_specialty_terms)}"),
+      str_glue("Paediatric by text (LFPSE):  Specialty does not contain {make_text_terms_pretty(adult_specialty_terms)} and text contains {make_text_terms_pretty(paediatric_terms)}")
+)
+
+  
+}
+
+
+writeData(wb, "Search strategy", neopaed_logic, startRow = 28, startCol = 5)
+addStyle(wb, "Search strategy", textStyle, rows = 28, cols = 5)
+addStyle(wb, "Search strategy", textStyle, rows = 40, cols = 5)
+
 
 
 
