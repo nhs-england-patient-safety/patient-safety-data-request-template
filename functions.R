1cc7cd06c494ef727700faeb117bb45bdbb624f2
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-05-28 17:44:57 +0100

    add error handling to is_multi_select

--- functions.R
+++ functions.R
@@ -648,10 +648,20 @@ make_text_terms_pretty <- function(term){
 
 
 is_multi_select<- function(df, variable_name){
-  n_multi<-df |> 
+  
+  if(variable_name %in% colnames(df)){
+    
+    n_multi<-df |> 
     select(all_of(c("col" = variable_name))) |>
     mutate(multi=str_detect(col,"; ")) |>
     filter(multi) |>
     nrow()
-  return(n_multi > 0)
+  
+    return(n_multi > 0)
+  
+  } else {
+    print("The column does not exist")
+    return(NA)
+}
+  
 }
