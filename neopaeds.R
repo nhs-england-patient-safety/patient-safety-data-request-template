5ecb45d751a881319e8cc853458d4787c517dacc
ruth-keane-nhs <ruth.keane1@nhs.net>
2025-06-13 10:26:48 +0100

    add | between school and child

--- neopaeds.R
+++ neopaeds.R
@@ -1,6 +1,6 @@
 # neopaeds search terms
 neonatal_terms <- "(?i)\\bn(?:\\W|)i(?:\\W|)c(?:\\W|)u\\b|\\bn(?:\\W|)n(?:\\W|)u\\b|\\bs(?:\\W|)c(?:\\W|)b(?:\\W|)u\\b|\\bneo(?:\\W|)nat|\\bbaby|\\bbabies|\\bnewborn"
 neonatal_specialty_terms <- "(?i)\\bneonat|\\bbaby"
-paediatric_terms <- "(?i)\\bp(?:\\W|)i(?:\\W|)c(?:\\W|)u\\b|\\bc(?:\\W|)a(?:\\W|)m(?:\\W|)h(?:\\W|)s\\b|\\bpaed|\\binfant|\\bschool\\bchild"
+paediatric_terms <- "(?i)\\bp(?:\\W|)i(?:\\W|)c(?:\\W|)u\\b|\\bc(?:\\W|)a(?:\\W|)m(?:\\W|)h(?:\\W|)s\\b|\\bpaed|\\binfant|\\bschool|\\bchild"
 paediatric_specialty_terms <- "(?i)\\bpaed|\\bchild"
 adult_specialty_terms <-  "(?i)\\badult|\\bold|\\belderly|geriat"
