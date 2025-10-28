3057514dfc46da65563172abe9bd500e6cf6c362
ruth-keane-nhs <142318599+ruth-keane-nhs@users.noreply.github.com>
2025-10-28 12:39:42 +0000

    move date_filter from functions to flow file

--- flow.R
+++ flow.R
@@ -1,6 +1,17 @@
 # flow
 tic <- Sys.time()
 
+
+#not a function- but this creates the object "date_filter" from date_type
+date_filter <- if (date_type == 'occurring') {
+  expr(occurred_date)
+} else if (date_type == 'reported') {
+  expr(reported_date)
+}
+
+
+
+
   if (search_nrls) {
     source('nrls.R')
   } else if (search_lfpse) {
