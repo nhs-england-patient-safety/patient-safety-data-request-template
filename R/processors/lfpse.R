be713ce3628def3fd5171837a46a65be39134fa0
Paul Johnson <paul.johnson50@nhs.net>
2025-11-19 17:42:22 +0000

    Add missing labelling and validation process

--- R/processors/lfpse.R
+++ R/processors/lfpse.R
@@ -147,31 +147,80 @@ if (check_and_log_empty_result(lfpse_filtered_text, dataset, "text")) {
   }
 } else {
   
+  lfpse_labelled<- lfpse_filtered_text |>
+    # pivot the coded columns
+    pivot_longer(cols = any_of(ResponseReference$QuestionId)) |>
+    # separate the multi-responses into single row per selection
+    separate_rows(value, sep = " {~@~} ") |>
+    # arrange so that multi-responses appear alphabetised later
+    arrange(value) |>
+    # bring through the value labels
+    left_join(ResponseReference, by = c(
+      "name" = "QuestionId",
+      "value" = "ResponseCode",
+      "TaxonomyVersion" = "TaxonomyVersion"
+    )) |>
+    # remove the unnecessary columns
+    select(!c(value, Property, LastUpdated, IsActive)) |>
+    # pivot back into columns
+    pivot_wider(
+      id_cols = !any_of(ResponseReference$QuestionId),
+      names_from = name,
+      values_from = ResponseText,
+      # collapse multi-responses into single row per entity
+      values_fn = list(ResponseText = ~ str_c(., collapse = "; "))
+    ) 
+  
+  #create a new column for age following validation 
+  lfpse_age_validated<- lfpse_labelled |>
+    mutate(age_unit = case_when(
+      is.na(P004_days) ~ 'age missing',
+      between(P004_days, 1, 30) ~ 'days',
+      between(P004_days, 31, 371) ~ 'months',
+      between(P004_days, 372, 74028) ~ 'years',
+      .default = 'age outside bounds')) |>
+    mutate(age_compliance = case_when(
+      age_unit == 'age outside bounds' ~ 'age outside bounds',
+      age_unit == 'age missing' ~ 'age missing',
+      age_unit == 'days' & between(P004_days, 1, 30) ~ 'yes',
+      age_unit == 'months' & P004_days %% 31 == 0 ~ 'yes',
+      age_unit == 'years' & P004_days %% 372 == 0 ~ 'yes',
+      .default = 'no')) |>
+    mutate(P004_days_validated = if_else(
+      age_compliance == "yes", P004_days, NA
+    ))
+  
   # age classification for neopaeds
-  lfpse_age_classified <- lfpse_filtered_text |>
+  lfpse_age_classified <- lfpse_age_validated |>
     mutate(
-      concat_col = paste(F001, AC001, OT003, A008_Other, A008, A002, DMD002, DMD004, sep = " "),
+      concat_col = paste(F001, AC001, OT003, A008_Other, L006, L006_Other, sep = " "),
+      
       P004_days_validated = ifelse(between(P004_days, 0, 365.25 * 120), P004_days, NA),
+      
       age_category = case_when(
-        between(P004_days_validated, 0, 28) ~ 'neonate',
-        between(P004_days_validated, 29, 365.25 * 18) ~ 'paediatric',
-        P004_days_validated >= 365.25 * 18 ~ 'adult',
+        (P004_days_validated > 0 & P004_days_validated <= 28) | 
+          (P007 %in% c("0-14 days", "15-28 days")) ~ "neonate",
+        (P004_days_validated > 28 & P004_days_validated < 6696) | 
+          (P007 %in% c("1-11 months", "1-4 years", "5-9 years", "10-15 years", "16 and 17 years")) ~ "paediatric",
         (!is.na(P007) | !is.na(P004_days_validated)) ~ 'adult estimated',
-        is.na(P004_days_validated) ~ 'unknown',
+        is.na(P004_days_validated) ~ 'unknown',# includes those where age is below zero / above believable threshold
         .default = 'other' 
       ),
+      
       neonate_specialty_flag = str_detect(L006, neonatal_specialty_terms),
       neonate_terms_flag = str_detect(concat_col, neonatal_terms),
       missing_specialty = is.na(L006),
       no_adult_specialty_flag = str_detect(L006, adult_specialty_terms, negate = TRUE), 
       paediatric_specialty_flag = str_detect(L006, paediatric_specialty_terms),
       paediatric_term_flag = str_detect(concat_col, paediatric_terms),
+      
       neonate_category = case_when(
         age_category == 'neonate' ~ "neonate_by_age",
         neonate_specialty_flag ~ "neonate_by_specialty",
         (neonate_terms_flag & (no_adult_specialty_flag | missing_specialty)) ~ "neonate_by_text",
         .default = "not neonate related"
       ),
+      
       paediatric_category = case_when(
         age_category == 'paediatric' ~ "paediatric_by_age",
         paediatric_specialty_flag ~ "paediatric_by_specialty",
